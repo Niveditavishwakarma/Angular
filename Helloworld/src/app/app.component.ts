22547003f5f21599c850f15c2e1c268c30c61cf7
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-06 16:27:08 +0530

    [Nivedita] Add . added the code of rendering the logo of bridgelabz

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -9,9 +9,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Helloworld';
-  imgUrl ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fyourstory.com%2Fcompanies%2Fbridgelabz-solutions&psig=AOvVaw3BbzY6Uk_fOvnh1HJyya5Q&ust=1741333931368000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMDY08X89IsDFQAAAAAdAAAAABAE";
+  imgUrl: string ="assets/download.png";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event : MouseEvent)
+  {
+      console.log("Save button is clicked!",$event);
+      window.open(this.url,"_blank");
   }
 }
