a5918ac7440d0bab9c5d24062ce96c19bb51a2cb
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-06 22:33:48 +0530

    [Nivedita] Fix . fixed the angular.json

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Helloworld';
-  imgUrl ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fyourstory.com%2Fcompanies%2Fbridgelabz-solutions&psig=AOvVaw3BbzY6Uk_fOvnh1HJyya5Q&ust=1741333931368000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMDY08X89IsDFQAAAAAdAAAAABAE";
+  imgUrl ="assets/download.png";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
