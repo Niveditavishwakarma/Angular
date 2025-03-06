180342add439e1a85569a48e3f5e61ac19d8a322
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-06 23:06:27 +0530

    [Nivedita] Add . added the code to redirect to the official page of bridgelabz by clicking the logo

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,12 +13,13 @@ export class AppComponent {
   title = 'Helloworld';
   imgUrl: string ="assets/download.png";
   url = "https://www.bridgelabz.com";
+  userName : string ="";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
   }
 
-  onClick($event : MouseEvent)
+  onClick($event : Event)
   {
     console.log("Save button is clicked!", $event);
     window.open(this.url,"_blank");
