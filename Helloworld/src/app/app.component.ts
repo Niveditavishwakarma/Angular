6c0fd99fd8ead55331eb2fd5cda3a6c932faf48d
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-03-07 10:55:38 +0530

    [Nivedita] Add . added the code to validate the username field

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -7,13 +7,14 @@ import { FormsModule } from '@angular/forms';
   standalone:true,
   imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Helloworld';
   imgUrl: string ="assets/download.png";
   url = "https://www.bridgelabz.com";
   userName : string ="";
+  nameError : string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -23,5 +24,17 @@ export class AppComponent {
   {
     console.log("Save button is clicked!", $event);
     window.open(this.url,"_blank");
+  }
+
+  onInput($event : Event)
+  {
+    console.log("Change Event Occured!",this.userName);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName))
+    {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";
   }
 }
